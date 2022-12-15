6d9464f9f6e8a71a75354f58831800235e2cc6db
Jessica Araujo <jessicacarolinedearaujo@outlook.com>
2022-12-14 21:32:29 -0300

    Adiciona pesquisa

--- co-women/src/components/search-bar/search-bar.js
+++ co-women/src/components/search-bar/search-bar.js
@@ -1,17 +1,25 @@
 import { InputAdornment, TextField } from "@mui/material";
 import Search from "@mui/icons-material/Search";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SearchBar() {
+  const [query, setQuery] = useState("");
+
   return (
     <TextField
       fullWidth={true}
       variant="outlined"
       type={"search"}
       placeholder="Pesquise por tipo de serviço"
+      value={query}
+      onChange={(e) => {
+        setQuery(e.target.value);
+        localStorage.setItem("query", e.target.value);
+      }}
       InputProps={{
         endAdornment: (
-          <Link to="/search" style={{ textDecoration: 'none' }}>
+          <Link to="/search" style={{ textDecoration: "none" }}>
             <InputAdornment position="end">
               <Search />
             </InputAdornment>
